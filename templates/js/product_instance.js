55462acc43d3a38006eefcc354a603c06022badc
Parteim <Sergej_kajnov@mail.ru>
2020-01-20 10:33:47 +0700

    JS

---
+++ templates/js/product_instance.js
@@ -0,0 +1,11 @@
+
+var count_btn_up = document.getElementById('count_btn_up');
+var counter_value = document.getElementById('count_of_things')
+
+count_btn_up.addEventListener('click', count_up);
+
+function count_up () {
+  // var count_number = parseInt(counter_value.value) + 1;
+  counter_value;
+  console.log(counter_value);
+}
